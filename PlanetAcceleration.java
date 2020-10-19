dde26154d77288ba8fe50f0763c95443d92df4c4
RRneg <71498446+RRneg@users.noreply.github.com>
2020-10-19 14:24:34 +0300

    Delete PlanetAcceleration.java

--- PlanetAcceleration.java
+++
@@ -1,9 +0,0 @@
-interface PlanetAcceleration {
-
-    default double acceleration(double m, double r) {
-        double g;
-        AccelerationUtil accelerationUtil = new AccelerationUtil();
-        g = accelerationUtil.freeFallAcceleration(m, r);
-        return g;
-    }
-}
