8c0c6305be35bdd2f88a5c86ca1df880367d6092
RRneg <71498446+RRneg@users.noreply.github.com>
2020-10-19 14:24:24 +0300

    Delete Planet.java

--- Planet.java
+++
@@ -1,6 +0,0 @@
-public abstract class Planet implements PlanetAcceleration {
-    public double m; // масса планеты
-    public double r; // радиус планеты
-    public int choice; // выбор планеты
-
-}
